07d2b92601c1894da3e7c5fe0601ec81f384ce5e
130oclock <aodan13@att.net>
2024-09-28 00:49:53 -0400

    Added Mandelbrot fragment shader

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 
-#define WIDTH 640u
-#define HEIGHT 480u
+#define WIDTH 1280u
+#define HEIGHT 720u
 
 #define SHADERNAME "mandelbrot_shader.glsl"
+#define MAX_ITERATIONS 1024
 
 int main() {
     // Check that shaders are available on this device.
@@ -20,16 +21,24 @@ int main() {
         exit(1);
     }
 
-    shader.setUniform("Width", (float)WIDTH);
-    shader.setUniform("Height", (float)HEIGHT);
-
     auto window = sf::RenderWindow{
         {WIDTH, HEIGHT},
         "Mandelbrot Set"
     };
     window.setFramerateLimit(60);
 
+    shader.setUniform("window_Shape", sf::Glsl::Vec2{window.getSize()});
+    shader.setUniform("MAX_ITERATIONS", MAX_ITERATIONS);
+
     auto screen = sf::RectangleShape{sf::Vector2f{window.getSize()}};
+
+    sf::RenderTexture tex;
+    tex.create(WIDTH, HEIGHT);
+    tex.draw(screen, &shader);
+    tex.display();
+
+    sf::Sprite background(tex.getTexture());
+
 
     while(window.isOpen()) {
         for (sf::Event event; window.pollEvent(event);) {
@@ -38,7 +47,7 @@ int main() {
         }
 
         window.clear(sf::Color::Black);
-        window.draw(screen, &shader);
+        window.draw(background);
         window.display();
     }
 
--- mandelbrot_shader.glsl
+++ mandelbrot_shader.glsl
@@ -1,21 +1,35 @@
-uniform float Width;
-uniform float Height;
+uniform vec2 window_Shape;
+uniform int MAX_ITERATIONS;
 
 
 void main() {
-    vec2 uv = vec2(gl_FragCoord.x / Width, gl_FragCoord.y / Height);
+    vec2 c = gl_FragCoord.xy / window_Shape;
 
-    // Scale everything within a circle of radius 2.
-    uv = uv - 0.5;
-    uv = uv * vec2(2, 2);
+    // Move (0, 0) to center of window.
+    c = c - 0.5;
 
-    // Adjust aspect ratio.
-    float aspect_ratio = Width / Height;
-    uv.x = uv.x * aspect_ratio;
+    // Adjust for aspect ratio.
+    float aspect_ratio = window_Shape.y / window_Shape.x;
+    c.y = c.y * aspect_ratio;
 
-    gl_FragColor = vec4(uv.x, 0.0, 0.0, 1.0);
+    c = c * 4.0;
 
-    if (uv.x * uv.x + uv.y * uv.y < 2.0) {
-        gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+    int i = 0;
+    vec2 z = vec2(0.0);
+
+    while (z.x * z.x + z.y * z.y < 4.0) {
+        float tmp = z.x * z.x - z.y * z.y + c.x;
+        z.y = 2.0 * z.x * z.y + c.y;
+        z.x = tmp;
+
+        if (i >= MAX_ITERATIONS) {
+            gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
+            return;
+        }
+        ++i;
     }
+
+    float color = float(i) / float(MAX_ITERATIONS);
+
+    gl_FragColor = vec4(color, color, color, 1.0);
 }
